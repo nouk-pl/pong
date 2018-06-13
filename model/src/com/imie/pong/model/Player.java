3732a961a0fcd0241d78ba12cbe37c4f3d477823
Audrey Patin <ladylithie@gmail.com>
2018-06-13 16:16:30 +0200

    initial model library

---
+++ model/src/com/imie/pong/model/Player.java
@@ -0,0 +1,24 @@
+/**
+ * 
+ */
+package com.imie.pong.model;
+
+/**
+ * @author Audrey
+ *
+ */
+public class Player {
+	private String pseudo;
+	private String side;
+	/**
+	 * @param pseudo
+	 * @param side
+	 */
+	public Player(String pseudo, String side) {
+		super();
+		this.pseudo = pseudo;
+		this.side = side;
+	}
+	
+	
+}
